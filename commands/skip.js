10b4de48930efa2af37f83fa3383a8f490a1680f
PatrickFM <85374813+PatrickFerreiraMoura@users.noreply.github.com>
2025-02-14 18:39:29 -0300

    Update skip.js

--- commands/skip.js
+++ commands/skip.js
@@ -53,7 +53,7 @@ async function skip(client, interaction, lang) {
 
 module.exports = {
     name: "skip",
-    description: "Skip the current song",
+    description: "Pula a música atual",
     permissions: "0x0000000000000800",
     options: [],
     run: skip
