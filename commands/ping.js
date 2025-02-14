5cbea8aa3c8696cd6d2b2b07ce6c11e0d4417bf3
PatrickFM <85374813+PatrickFerreiraMoura@users.noreply.github.com>
2025-02-14 18:31:34 -0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -4,7 +4,7 @@ const musicIcons = require('../UI/icons/musicicons.js');
 
 module.exports = {
   name: "ping",
-  description: "Check the bot latency",
+  description: "Verifica a latência do bot",
   permissions: "0x0000000000000800",
   options: [],
   run: async (client, interaction, lang) => {
