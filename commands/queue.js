734ccd95e23564a5dc7f8d78d0ecb2a8e2129727
PatrickFM <85374813+PatrickFerreiraMoura@users.noreply.github.com>
2025-02-14 18:36:29 -0300

    Update queue.js

--- commands/queue.js
+++ commands/queue.js
@@ -93,7 +93,7 @@ async function queue(client, interaction, lang) {
 
 module.exports = {
     name: "queue",
-    description: "Show the current song queue",
+    description: "Mostra a fila de músicas atual",
     permissions: "0x0000000000000800",
     options: [],
     run: queue
