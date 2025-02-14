e4fe7d5cde413323dc2f4ffd45df761c6688de48
PatrickFM <85374813+PatrickFerreiraMoura@users.noreply.github.com>
2025-02-14 18:29:46 -0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -6,7 +6,7 @@ const musicIcons = require('../UI/icons/musicicons.js');
 
 module.exports = {
   name: "help",
-  description: "Get information about the bot",
+  description: "Obtenha informações sobre o bot",
   permissions: "0x0000000000000800",
   options: [],
   run: async (client, interaction, lang) => {
