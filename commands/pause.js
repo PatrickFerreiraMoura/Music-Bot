ce73632a85e70cb145dc7e27fc8472a5a430cecb
PatrickFM <85374813+PatrickFerreiraMoura@users.noreply.github.com>
2025-02-14 18:31:06 -0300

    Update pause.js

--- commands/pause.js
+++ commands/pause.js
@@ -53,7 +53,7 @@ async function pause(client, interaction, lang) {
 
 module.exports = {
     name: "pause",
-    description: "Pause the current song",
+    description: "Pausa a música atual",
     permissions: "0x0000000000000800",
     options: [],
     run: pause
